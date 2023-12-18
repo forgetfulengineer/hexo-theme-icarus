193ff7a9c4875a60b5f2223ec8e12bd35aeb1fa4
forgetfulengineer <thatforgetfulengineer@gmail.com>
2023-12-19 00:13:32 +0800

    個人資訊 - 隱藏頭貼&手機版隱藏個人資訊

--- layout/widget/profile.jsx
+++ layout/widget/profile.jsx
@@ -29,14 +29,14 @@ class Profile extends Component {
             followTitle,
             socialLinks
         } = this.props;
-        return <div class="card widget" data-type="profile">
+        return <div class="card widget is-hidden-mobile" data-type="profile">
             <div class="card-content">
                 <nav class="level">
                     <div class="level-item has-text-centered flex-shrink-1">
                         <div>
-                            <figure class="image is-128x128 mx-auto mb-2">
+                            {/* <figure class="image is-128x128 mx-auto mb-2">
                                 <img class={'avatar' + (avatarRounded ? ' is-rounded' : '')} src={avatar} alt={author} />
-                            </figure>
+                            </figure> */}
                             {author ? <p class="title is-size-4 is-block" style={{'line-height': 'inherit'}}>{author}</p> : null}
                             {authorTitle ? <p class="is-size-6 is-block">{authorTitle}</p> : null}
                             {location ? <p class="is-size-6 is-flex justify-content-center">
