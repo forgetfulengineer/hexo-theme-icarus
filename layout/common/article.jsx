03dd9f6de4e97bc8ce0e8597cc1e6880d783b128
forgetfulengineer <thatforgetfulengineer@gmail.com>
2023-12-28 13:39:40 +0800

    文章佈局 - page.layout = page 不顯示版權區塊

--- layout/common/article.jsx
+++ layout/common/article.jsx
@@ -115,7 +115,7 @@ module.exports = class extends Component {
                     {/* Content/Excerpt */}
                     <div class="content" dangerouslySetInnerHTML={{ __html: index && page.excerpt ? page.excerpt : page.content }}></div>
                     {/* Licensing block */}
-                    {!index && article && article.licenses && Object.keys(article.licenses)
+                    {!index && page.layout !== 'page' && article && article.licenses && Object.keys(article.licenses)
                         ? <ArticleLicensing.Cacheable page={page} config={config} helper={helper} /> : null}
                     <hr/>
                     <div class="level is-flex">
