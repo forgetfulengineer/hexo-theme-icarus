4717fff4b0ca31c2b7ffb53deb97e9decd125197
forgetfulengineer <thatforgetfulengineer@gmail.com>
2025-10-09 17:42:36 +0800

    載入速度優化 - 調整 preconnect 和 preconnect 的內容

--- layout/common/head.jsx
+++ layout/common/head.jsx
@@ -162,15 +162,15 @@ module.exports = class extends Component {
                 updated={page.updated}
                 images={structuredImages} /> : null}
 
+            <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin/>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://cdn.jsdelivr.net"/>
+            <link rel="preconnect" href="https://forgetfulengineer-ga-data.netlify.app"/>
+            {is_post(page) ? <link rel='preconnect' href='https://forgetfulengineer-twikoo.netlify.app/' crossorigin/> : null}
             <link rel="preload" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.0.0/webfonts/fa-solid-900.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
             <link rel="preload" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.0.0/webfonts/fa-regular-400.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
             <link rel="preload" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.0.0/webfonts/fa-brands-400.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
             <link rel="preload" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.0.0/css/all.min.css" as="style" onload="this.rel='stylesheet'" crossorigin="anonymous"/>
-            <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin/>
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            {is_post(page) ? <link rel='preconnect' href='https://forgetfulengineer-twikoo.netlify.app/' crossorigin/> : null}
-            {is_post(page) ? <link rel='preconnect' href='https://codepen.io/'/> : null}
-            {is_post(page) ? <link rel='preconnect' href='https://onecompiler.com/'/> : null}
             {canonical_url ? <link rel="canonical" href={canonical_url} /> : null}
             {rss ? <link rel="alternate" href={url_for(rss)} title={config.title} type="application/atom+xml" /> : null}
             {favicon ? <link rel="icon" href={url_for(favicon)} /> : null}
